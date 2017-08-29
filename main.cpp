cc84c5bb718b3477719c5c2583a9adde8bd616df
Bruno FS <bfs15@inf.ufpr.br>
2017-08-29 14:01:54 -0300

    Cleaned refining

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ void subst(Matrix& coefs, vector<double>& var_x, vector<double>& indTerms, bool 
 	long i, j;
 	int step;
 	long size = coefs.size;
-    
+
 	if (forward){
 		i = 1;
 		step = +1;
@@ -82,7 +82,7 @@ void subst(Matrix& coefs, vector<double>& var_x, vector<double>& indTerms, bool 
 	long i, j;
 	int step;
 	long size = coefs.size;
-    
+
 	if (forward){
 		i = 1;
 		step = +1;
@@ -177,11 +177,46 @@ void Cholesky(Matrix& a, Matrix& l, Matrix& u) {
 	}
 	/**/
 }
+
+////////////////////////////////////////////////////////////////////////////////
+//  int Crout_LU_Decomposition(double *A, int n)                              //
+//                                                                            //
+//  Description:                                                              //
+//     This routine uses Crout's method to decompose the n x n matrix A       //
+//     into a lower triangular matrix L and a unit upper triangular matrix U  //
+//     such that A = LU.                                                      //
+//     The matrices L and U replace the matrix A so that the original matrix  //
+//     A is destroyed.                                                        //
+//     Note!  In Crout's method the diagonal elements of U are 1 and are      //
+//            not stored.                                                     //
+//     Note!  The determinant of A is the product of the diagonal elements    //
+//            of L.  (det A = det L * det U = det L).                         //
+//     This routine is suitable for those classes of matrices which when      //
+//     performing Gaussian elimination do not need to undergo partial         //
+//     pivoting, e.g. positive definite symmetric matrices, diagonally        //
+//     dominant band matrices, etc.                                           //
+//     For the more general case in which partial pivoting is needed use      //
+//                    Crout_LU_Decomposition_with_Pivoting.                   //
+//     The LU decomposition is convenient when one needs to solve the linear  //
+//     equation Ax = B for the vector x while the matrix A is fixed and the   //
+//     vector B is varied.  The routine for solving the linear system Ax = B  //
+//     after performing the LU decomposition for A is Crout_LU_Solve          //
+//     (see below).                                                           //
+//                                                                            //
+//     The Crout method is given by evaluating, in order, the following       //
+//     pair of expressions for k = 0, ... , n - 1:                            //
+//       L[i][k] = (A[i][k] - (L[i][0]*U[0][k] + . + L[i][k-1]*U[k-1][k]))    //
+//                                 for i = k, ... , n-1,                      //
+//       U[k][j] = A[k][j] - (L[k][0]*U[0][j] + ... + L[k][k-1]*U[k-1][j])    //
+//                                                                  / L[k][k] //
+//                                      for j = k+1, ... , n-1.               //
+//       The matrix U forms the upper triangular matrix, and the matrix L     //
 /* */
-void crout(Matrix& a, Matrix& l, Matrix& u) {
+int crout(Matrix& a, Matrix& l, Matrix& u) {
 	long k, i, j, p, size = a.size;
 	double sum;
-	/**/
+
+	/**
 	for(k = 0; k <= size-1; k++){
 		u.at(k,k) = 1;
 		for(i = k; i <= size-1; i++){
@@ -220,7 +255,54 @@ void crout(Matrix& a, Matrix& l, Matrix& u) {
 			}
 		}
 	}
+	/*For each row and column, k = 0, ..., n-1,
+	find the lower triangular matrix elements for column k
+	and if the matrix is non-singular (nonzero diagonal element).
+	find the upper triangular matrix elements for row k. */
+	/**
+	int row;
+	double *p_k, *p_row, *p_col, *A = &a.matrix[0];
+	long n = a.size;
+	
+	for (k = 0, p_k = A; k < n; p_k += n, k++) {
+		for (i = k, p_row = p_k; i < n; p_row += n, i++) {
+			for (p = 0, p_col = A; p < k; p_col += n, p++)
+				*(p_row + k) -= *(p_row + p) * *(p_col + k);
+		}  
+		if ( *(p_k + k) == 0.0 ) return -1;
+		for (j = k+1; j < n; j++) {
+			for (p = 0, p_col = A; p < k; p_col += n,  p++)
+				*(p_k + j) -= *(p_k + p) * *(p_col + j);
+			*(p_k + j) /= *(p_k + k);
+		}
+	}
+	/* *
+	for (k = 0; k <= size-1; k++){
+		for (i = 0; i <= size-1; i++){
+			if (i < k){
+				l.at(i,k) = 0;
+			}else{
+				l.at(i,k) = a.at(i,k);
+				for (p = 0; p < k; p++){
+					l.at(i,k) = l.at(i,k) - l.at(i,p) * u.at(p,k);
+				}
+			}
+		}
+		for (i = 0; i <= size-1; i++){
+			if (i < k){
+				u.at(k,i) = 0;
+			}else if (i == k){
+				u.at(k,i) = 1;
+			}else{
+				u.at(k,i) = a.at(k,i) / l.at(k,k);
+				for (p = 0; p < k; p++){
+					u.at(k,i) = u.at(k,i) - ((l.at(k,p) * u.at(p,i)) / l.at(k,k));
+				}
+			}
+	  }
+	}
 	/**/
+	return 0;
 }
 
 void JacobiIt(Matrix& coefs, vector<double>& indTerms, vector<double>& x){
@@ -241,7 +323,7 @@ void JacobiIt(Matrix& coefs, vector<double>& indTerms, vector<double>& x){
 
 vector<double> found_values(Matrix coefs, vector<double> x){
     vector<double> values(x.size());
-    
+
     for(long i=0; i <= x.size()-1; i++){
         values.at(i) = 0;
         for(long j=0; j <= x.size()-1; j++){
@@ -255,23 +337,44 @@ void solve_lu(Matrix l, Matrix u, vector<double>& x, vector<double>& b, vector<l
 	vector<double> z(x.size());
 	//* find z; LZ=b *
     subst(l, z, b, true, P);
-
-	cout<< endl <<"Z is"<< endl;
-	printv(z);
-	cout<< endl;
-	
+	//* find x; Ux=z *
     subst(u, x, z, false);
 }
 
+
+
 template <class T>
 vector<T> add_vec(vector<T> a, vector<T> b, int sign = 1){
-    vector<T> x(a > b ? a.size() : b.size());
-    
-    for(long i=0; i <= x.size(); i++){
+    vector<T> x(a.size());
+
+    for(long i=0; i <= x.size()-1; i++){
         x.at(i) = a.at(i) + sign*b.at(i);
     }
-    
+
     return x;
+}
+
+void lu_refining(Matrix coef, Matrix l, Matrix u, vector<vector<double>>& x, vector<double>& b, vector<long>& P){
+	vector<double> values(b.size()), r(b.size()), w(b.size());
+
+	cout<<"\n\nRefining "<< endl;
+	for(long i=0; i <= 3; i++) {
+    	values = found_values(coef, x.at(i));
+    	// r: residue of x
+    	r = add_vec(b, values, -1);
+		cout<<"\n\n System Residue "<< i << endl;
+		printv(r);
+
+    	// w: residues of each variable of x
+    	solve_lu(l, u, w, r, P);
+
+		cout<<"\n Variables Residue "<< i << endl;
+		printv(w);
+
+		x.push_back(vector<double>(b.size()));
+    	// adjust x with found errors
+    	x.at(i+1) = add_vec(x.at(i), w);
+	}
 }
 
 int main(int argc, char **argv) {
@@ -286,8 +389,8 @@ int main(int argc, char **argv) {
 	Matrix coef(size, 0), l(size, 0), u(size, 0);
 	vector<double> b(size), z(size);
 	vector<long> P(size);
-	vector<vector<double>> x(5);
-	x.at(0).resize(size);
+	vector<vector<double>> x;
+	x.push_back(vector<double>(size));
 
 	cout<<"Enter all coefficients of matrix : ";
 	for(i=0; i<=size-1; i++){
@@ -302,61 +405,22 @@ int main(int argc, char **argv) {
 	//crout(coef, l, u);
 	//Cholesky(coef, l, u);
 	GaussEl(coef, l, u, P);
-	/* *
-	for (i = 0; i <= size-1; i++){
-		for (j = 0; j <= size-1; j++){
-			if (j < i){
-				l.at(j,i) = 0;
-			}else{
-				l.at(j,i) = coef.at(j,i);
-				for (k = 0; k < i; k++){
-					l.at(j,i) = l.at(j,i) - l.at(j,k) * u.at(k,i);
-				}
-			}
-		}
-		for (j = 0; j <= size-1; j++){
-			if (j < i){
-				u.at(i,j) = 0;
-			}else if (j == i){
-				u.at(i,j) = 1;
-			}else{
-				u.at(i,j) = coef.at(i,j) / l.at(i,i);
-				for (k = 0; k < i; k++){
-					u.at(i,j) = u.at(i,j) - ((l.at(i,k) * u.at(k,j)) / l.at(i,i));
-				}
-			}
-	  }
-	}
-	/**/
 
 	//* Displaying LU matrix *
-	cout<< endl << endl <<"L matrix is "<< endl;
+	cout<<"\n\nL matrix is "<< endl;
 	printm(l);
-	cout<< endl <<"U matrix is "<< endl;
+	cout<<"\nU matrix is "<< endl;
 	printm(u);
-	cout<< endl <<"Pivoting Permutaton is "<< endl;
+	cout<<"\nPivoting Permutaton is "<< endl;
 	printv(P);
 
 	//* find first iteration of x *
 	solve_lu(l, u, x.at(0), b, P);
-	
-	vector<double> values(x.size()), r(x.size()), w(x.size());
-	for(long i=1; i <= 3 ; i++) {
-    	values = found_values(coef, x.at(i));
-    	// r: residue of x
-    	r = add_vec(b, values, -1);
-    	
-    	// w: residues of each variable of x
-    	solve_lu(l, u, w, r, P);
-    	
-    	x.at(i+1).resize(size);
-    	// adjust x with found errors
-    	x.at(i+1) = add_vec(x.at(i), w);
-	}
-	
 
-	cout<<endl<<"Set of solution is"<<endl;
-	//printv(x.at(x.size()-1));
+	lu_refining(coef, l, u, x, b, P);
+
+	cout<<"\nSet of solution is"<<endl;
+	printv(x.at(x.size()-1));
 	cout << endl;
 
 	return 0;

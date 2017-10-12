fcd22b986f5a256bc12f6e373321b400bc0036c7
Bruno FS <bfs15@inf.ufpr.br>
2017-10-12 01:00:52 -0300

    started

--- src/GaussEl.h
+++ src/GaussEl.h
@@ -19,15 +19,15 @@ void GaussEl(Matrix A, Matrix& LU, vector<long>& P) {
 	LU.resize(A.size);
 	LU.set(A);
 	// initializing permutation vector
-	for(long i = 0; i <= LU.size-1; i++){
+	for(long i = 0; i < LU.size; i++){
 		P.at(i) = i;
 	}
 
-	for(long p = 0; p <= LU.size-1; p++){
+	for(long p = 0; p < LU.size; p++){
 		// for each pivot
 		/* partial pivoting */
 		long maxRow = p;
-		for(long i = p+1; i <= LU.size-1; i++){
+		for(long i = p+1; i < LU.size; i++){
 			// for each value below the p pivot
 			if(abs(LU.at(i,p)) > abs(LU.at(maxRow,p))) maxRow = i;
 		} // finds max value
@@ -40,7 +40,7 @@ void GaussEl(Matrix A, Matrix& LU, vector<long>& P) {
 			fprintf(stderr, "Found a pivot == 0, system is not solvable with partial pivoting");
 			exit(EXIT_FAILURE);
 		}
-		//for (long i = p+1; i <= LU.size-1; i++) {	//going from below pivot to end
+		//for (long i = p+1; i < LU.size; i++) {	//going from below pivot to end
 		for (long i = LU.size-1; i >= p+1; i--) {	//going from end to pivot
 			// for each line below pivot
 			if (!close_zero(LU.at(i,p))){
@@ -48,7 +48,7 @@ void GaussEl(Matrix A, Matrix& LU, vector<long>& P) {
 				// find pivot multiplier, store in L
 				LU.at(i, p) = LU.at(i, p)/LU.at(p, p);
 				// subtract pivot from current line (in U)
-				for (long k = p+1; k <= LU.size-1; k++) {
+				for (long k = p+1; k < LU.size; k++) {
 					// for each collumn starting from pivot's
 					LU.at(i, k) -= LU.at(p, k) * LU.at(i, p);
 					// mulitply pivot line value to multiplier
--- src/Matrix.h
+++ src/Matrix.h
@@ -5,6 +5,9 @@
 */
 namespace std {
 
+#define mod(X,Y) (((X) % (Y)) < 0 ? ((X) % (Y)) + (Y) : ((X) % (Y)))
+#define CACHE_LINE_SIZE 8
+#define PAD(X) ((X)+1)/CACHE_LINE_SIZE*(CACHE_LINE_SIZE*(CACHE_LINE_SIZE-1))/2
 /**
  * @brief Stores values of matrix in a vector, Row Major Order
  */
@@ -35,7 +38,7 @@ public:
 	void swap_rows_from(long row0, long row1, long start){
 		if(row0 == row1)
 			return;
-		for(long j = start; j <= size-1; j++){
+		for(long j = start; j < size; j++){
 			// for each collumn
 			swap(this->at(row0, j), this->at(row1, j));
 		}
@@ -55,8 +58,8 @@ public:
 	 * @param sign -1 with you want to add -b
 	 */
 	void add(Matrix& b, double sign = 1){
-		for(long i=0; i <= size-1; i++){
-			for(long j=0; j <= size-1; j++){
+		for(long i=0; i < size; i++){
+			for(long j=0; j < size; j++){
 				this->at(i,j) += sign*b.at(i,j);
 			}
 		}
@@ -65,8 +68,8 @@ public:
 	 * @brief copy matrix M to yourself
 	 */
 	void set(Matrix& M){
-		for(long i=0; i <= size-1; i++){
-			for(long j=0; j <= size-1; j++){
+		for(long i=0; i < size; i++){
+			for(long j=0; j < size; j++){
 				this->at(i,j) = M.at(i,j);
 			}
 		}
@@ -80,18 +83,6 @@ public:
 				cout << this->at(i, j) <<'\t';
 			}
 			cout << endl;
-		}
-	}
-	/**
-	 * @brief prints matrix coeficients together with B
-	 * @param B
-	 */
-	void print(vector<double>& B){
-		for (long i = 0; i < size; i++) {
-			for (long j = 0; j < size; j++) {
-				cout << this->at(i, j) <<"x"<< j <<'\t';
-			}
-			cout <<"=\t"<< B[i] << endl;
 		}
 	}
 };
@@ -146,8 +137,8 @@ void randomMatrix(Matrix& M){
  */
 void printm(Matrix& matrix){
 	cout<<  matrix.size <<"\n";
-	for(long i = 0; i <= matrix.size-1; i++){
-		for(long j = 0; j <= matrix.size-1; j++)
+	for(long i = 0; i < matrix.size; i++){
+		for(long j = 0; j < matrix.size; j++)
 			cout<< matrix.at(i,j) <<"\t";
 		cout<< endl;
 	}
@@ -160,6 +151,128 @@ template <class T>
 void printv(vector<T>& x){
 	for(T& vx : x)
 		cout << vx <<'\t';
+}
+
+
+/**
+ * @brief Stores values of a triangular matrix in a array, each line goes only until elements of the diagonal
+ * ex: having ​​CACHE_LINE_SIZE = ​3​
+ *   0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6  7  8  9  0  1  2
+ * [00, -​, -​|10,11​, -​|20,21,22|30,31,32,33​, -, -​|40,41,42,43,44, -|​50,...​]
+ * pad=​2​;​   pad=​1​;   pad=​0;   ​pad=​2​;​            ​pad=​1​;​            pad=0;​
+ * +2      ​ +1​       +0       +2​ ​               +1​​                +0
+ * // having i​​ = 4;
+ * // 3 = ​(5/3 -2/3)*  3*2/2​; // ok​
+ * padding_before = (i+1)/​​​CACHE_LINE_SIZE *(​​​​​CACHE_LINE_SIZE *(​CACHE_LINE_SIZE-1)/2);
+ * a1 = ​​​CACHE_LINE_SIZE​ - mod(i,​​CACHE_LINE_SIZE​)​;
+ * sum = (​CACHE_LINE_SIZE​ ​- a1)*(a1+CACHE_LINE_SIZE​-1​)/2;
+ * sum = mod(i,​​CACHE_LINE_SIZ​​E​)*(​​2*CACHE_LINE_SIZE​ ​​-1 - mod(i,​​CACHE_LINE_SIZE​)​​)/2​;
+ * // 2 = mod(4,3)*(2*3 -1 - mod(4,3))/2; // ok
+ * ​pad_btotal = ​padding_before​+​​sum 
+ * [i*(i+1)/2​ + ​​pad_btota​l​​ + j​​]​
+ */
+class MatrixTriSeq
+{
+public:
+	double* matrix;
+	long size;
+	/**
+	 * @param size of matrix, total number of lines
+	 */
+	MatrixTriSeq(long size){
+		malloc(size);
+	}
+
+	MatrixTriSeq(){
+	}
+	/**
+	 * @param i
+	 * @param j
+	 * @return element of position i,j
+	 */
+	double& at(long i, long j) {
+		long sum = mod(i, CACHE_LINE_SIZE)*(2*CACHE_LINE_SIZE -1 - mod(i, CACHE_LINE_SIZE))/2;
+		long pad_btotal = PAD(i) + sum;
+		return matrix[i*(i+1)/2 + pad_btotal + j];
+		//i*(i+1)/2​ + (i+1)*​​​(x-1)/2 ​+ mod(i,​x​)*(​​2*x ​​-1 - mod(i,​x​)​​)/2​ + j​;
+	}
+
+	void swap_rows(long row0, long row1){
+		swap_rows_from(row0, row1, 0);
+	}
+
+	void malloc(long new_size){
+		size = new_size;
+		matrix = malloc((size*size + PAD(size))*sizeof(double));
+	}
+	/**
+	 * @brief Increments b into the matrix
+	 * @param b
+	 * @param sign -1 with you want to add -b
+	 */
+	void add(Matrix& b, double sign = 1){
+		for(long i=0; i < size; i++){
+			for(long j=0; j < i+1; j++){
+				this->at(i,j) += sign*b.at(i,j);
+			}
+		}
+	}
+	/**
+	 * @brief copy matrix M to yourself
+	 */
+	void set(Matrix& M){
+		for(long i=0; i < size; i++){
+			for(long j=0; j < i+1; j++){
+				this->at(i,j) = M.at(i,j);
+			}
+		}
+	}
+
+	/**
+	 * @brief prints matrix
+	 */
+	void print(){
+		for(long i = 0; i < size; i++){
+			for(long j = 0; j < i+1; j++){
+				cout << this->at(i, j) <<'\t';
+			}
+			for(long j = i+1; j < size; j++){
+				cout << (double)0.0 <<'\t';
+			}
+			cout << endl;
+		}
+	}
+};
+
+/**
+ * @brief swaps rows starting from col 'start'
+ */
+void swap_rows(MatrixTriSeq& Low, MatrixTriSeq& Upp, long row0, long row1){
+	if(row0 == row1)
+		return;
+	
+	if(row0 > row1){swap(row0, row1);}
+	/* 6 1 2
+	 * 3 4 5
+	 * 0 7 8 */
+	for(long j = 0; j < row0+1; j++){
+		// for each collumn
+		swap(Low.(row1, j), Low.at(row0, j));
+	}
+	/* 6 7 2
+	 * 3 4 5
+	 * 0 1 8 */
+	for(long j = row0+1; j < row1+1; j++){
+		// for each collumn
+		swap(Upp.at(row0, j), Low.at(row1, j));
+	}
+	/* 6 7 8
+	 * 3 4 5
+	 * 0 1 2 */
+	for(long j = row1; j < size; j++){
+		// for each collumn
+		swap(Upp.at(row0, j), Upp.at(row1, j));
+	}
 }
 
 

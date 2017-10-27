ef4957dbc44dcb22401b5a545c71da41d7b9b079
Aisu Wata <bfs15@inf.ufpr.br>
2017-10-27 08:06:22 -0200

    varray fix

--- src/t_vector.h
+++ src/t_vector.h
@@ -1,8 +1,8 @@
 
 void t_vector(long size){
 	Matrix LU(size);
-	vector<double> X(LU.m_size);
-	MatrixColMajor B(size);
+	varray<double> X(LU.m_size);
+	varray<double> B(LU.m_size);
 	
 	long bi, bk;
 	long i, j, k;
@@ -14,9 +14,9 @@ void t_vector(long size){
 	asm("Setup");
 	for(i = 0; i < size; i++){
 		X.at(i) = 0;
+		B.at(i) = i;
 		for(vj = 0; vj < size/dn; vj += 1){
 			vec(dj) LU.at(i, vj*dn+dj) = (i*size + vj*dn+dj);
-			B.atv(vj,i) = LU.atv(i,vj);
 		}
 	}
 	asm("END Setup");
@@ -35,19 +35,18 @@ void t_vector(long size){
 		for(bi = 0; bi < size; bi += BL1){
 			unroll(ui,BL1) X.at(bi+ui) = 0;
 			for(bk = 0; bk < size; bk += BL1){
-				asm("VECmult_Tilroll inner");
+				//asm("VECmult_Tilroll inner");
 				for(i = bi; i < (bi + BL1); i += unr){
 					v<double> acc[unr];
-					//v<double> acc[unr];
 					//memset(acc, 0, sizeof(acc));
 					unroll(ui,unr) vec(d) acc[ui].v[d] = 0;
 					for(vk = bk/dn; vk < (bk + BL1)/dn; vk++){
-						unroll(ui,unr) acc[ui].v = acc[ui].v + LU.atv(i+ui,vk).v * B.atv(vk,0).v;
+						unroll(ui,unr) acc[ui].v = acc[ui].v + LU.atv(i+ui,vk).v * B.atv(vk).v;
 					}
 					unroll(ui,unr)
 						vec(d) X.at(i+ui) += acc[ui].v[d];
 				}
-				asm("END VECmult_Tilroll inner");
+				//asm("END VECmult_Tilroll inner");
 			}
 		}
 		asm("END VECmult_Tilroll");
@@ -66,7 +65,7 @@ void t_vector(long size){
 				for(i = bi; i < (bi + BL1); i++){
 					v<double> acc = {0};
 					for(vk = bk/dn; vk < (bk + BL1)/dn; vk++){
-						acc.v = acc.v + LU.atv(i,vk).v * B.atv(vk,0).v;
+						acc.v = acc.v + LU.atv(i,vk).v * B.atv(vk).v;
 					}
 					vec(d) X.at(i) += acc.v[d];
 				}
@@ -87,7 +86,7 @@ void t_vector(long size){
 			unroll(ui,unr)
 				vec(d) acc[ui].v[d] = 0;
 			for(k = 0; k < size/dn; k++){
-				unroll(ui,unr) acc[ui].v = acc[ui].v + LU.atv(i+ui,k).v * B.atv(k,0).v;
+				unroll(ui,unr) acc[ui].v = acc[ui].v + LU.atv(i+ui,k).v * B.atv(k).v;
 			}
 			unroll(ui,unr) X.at(i+ui) = 0;
 			unroll(ui,unr)
@@ -106,7 +105,7 @@ void t_vector(long size){
 		for(i = 0; i < size; i++){
 			v<double> acc = {0};
 			for(k = 0; k < size/dn; k++){
-				acc.v = acc.v + LU.atv(i,k).v * B.atv(k,0).v;
+				acc.v = acc.v + LU.atv(i,k).v * B.atv(k).v;
 			}
 			X.at(i) = 0;
 			vec(d) X.at(i) += acc.v[d];
@@ -124,7 +123,7 @@ void t_vector(long size){
 		for(i = 0; i < size; i++){
 			double acc[dn] = {0};
 			for(k = 0; k < size/dn; k++){
-				vec(d) acc[d] = acc[d] + LU.at(i, k*4+d) * B.at(k*4+d, 0);
+				vec(d) acc[d] = acc[d] + LU.at(i, k*4+d) * B.at(k*4+d);
 			}
 			X.at(i) = 0;
 			vec(d) X.at(i) += acc[d];
@@ -142,7 +141,7 @@ void t_vector(long size){
 		for(i = 0; i < size; i++){
 			X.at(i) = 0;
 			for(k = 0; k < size; k++){
-				X.at(i) = X.at(i) + LU.at(i, k) * B.at(k, 0);
+				X.at(i) = X.at(i) + LU.at(i, k) * B.at(k);
 			}
 		}
 		asm("END Naive");
@@ -152,5 +151,5 @@ void t_vector(long size){
 	if(PRINT_MATRIX) { printv(X); cout << endl; }
 	t[c++] = clock();
 	
-	cout << X.at(0) <<":"<< X.at(X.size()-1) <<"\tend size "<< size << endl;
+	cout << X.at(0) <<":"<< X.at(X.size-1) <<"\tend size "<< size << endl;
 }

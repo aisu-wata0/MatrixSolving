5605ccf1bfef8cdd1f24958c96c9e8f828e5146a
Aisu Wata <aisu.wata0@gmail.com>
2017-10-27 11:26:34 -0200

    fixed lik.sh

--- lik.sh
+++ lik.sh
@@ -1,1 +1,3 @@
-likwid-perfctr -C0 -V -oout.txt -O -gCACHE -m $1
+echo 'likwid-perfctr -C 1  -f  -o $1 -O -g $2 -m $3'
+echo likwid-perfctr -C 1  -f  -o $1 -O -g $2 -m $3
+likwid-perfctr -C 1  -f  -o $1 -O -g $2 -m $3

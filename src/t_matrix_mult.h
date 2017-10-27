22a3af7d4024410f60f979b52f1202eeaaba531c
Aisu Wata <bfs15@inf.ufpr.br>
2017-10-27 08:58:30 -0200

    woking && cleaned

--- src/t_matrix_mult.h
+++ src/t_matrix_mult.h
@@ -1,9 +1,9 @@
 
 
 #define block(i,i0,imax, j,j0,jmax, k,k0,kmax, bstep) \
-for (size_t i = i0; i < imax; i += bstep) \
-	for (size_t j = j0; j < jmax; j += bstep) \
-		for (size_t k = k0; k < kmax; k += bstep)
+for (i = i0; i < imax; i += bstep) \
+	for (j = j0; j < jmax; j += bstep) \
+		for (k = k0; k < kmax; k += bstep)
 
 //inline void test(Matrix& LU, MatrixColMajor& B, Matrix& X){
 void t_matrix_mult(size_t size){
@@ -35,6 +35,7 @@ void t_matrix_mult(size_t size){
 	step = +1;
 	//bstep = step * BL1/3; // cache block size
 	size_t bi[5], bj[5], bk[5];
+	size_t bimax[5], bjmax[5], bkmax[5];
 	size_t bstep[5];
 	bstep[0] = 8;
 	bstep[1] = bstep[0]*3;
@@ -78,39 +79,12 @@ void t_matrix_mult(size_t size){
 	set(X,0);
 	timer.tick();
 	asm("Tiled0");
-	for (size_t i0 = 0; i0 < size; i0 += bstep[0]) {
-		for (size_t j0 = 0; j0 < size; j0 += bstep[0]) {
-			for (size_t k0 = 0; k0 < size; k0 += bstep[0]) {
-				size_t imax = i0 + bstep[0] > size ? size : i0 + bstep[0];
-				size_t jmax = j0 + bstep[0] > size ? size : j0 + bstep[0];
-				size_t kmax = k0 + bstep[0] > size ? size : k0 + bstep[0];
-
-				for (size_t j = j0; j < jmax; ++j) {
-					for (size_t i = i0; i < imax; ++i) {
-						for (size_t k = k0; k < kmax; ++k) {
-							X.at(i, j) = X.at(i, j) + LU.at(i, k) * B.at(k, j);
-						}
-					}
-				}
-			}
-		}
-	}
-	asm("END Tiled0");
-	printf("Tiled0: %f sec\n", timer.tick(&cl));
-	if(PRINT_MATRIX) { printm(X); cout << endl; }
-	/**
-	
-	
-	set(X,0);
-	timer.tick();
-	asm("Tiled0");
-	for (size_t bi[0] = 0; bi[0] < size; bi[0] += bstep[0]) {
-		for (size_t bj[0] = 0; bj[0] < size; bj[0] += bstep[0]) {
-			for (size_t bk[0] = 0; bk[0] < size; bk[0] += bstep[0]) {
+	for (bi[0] = 0; bi[0] < size; bi[0] += bstep[0]) {
+		for (bj[0] = 0; bj[0] < size; bj[0] += bstep[0]) {
+			for (bk[0] = 0; bk[0] < size; bk[0] += bstep[0]) {
 				size_t imax = bi[0] + bstep[0] > size ? size : bi[0] + bstep[0];
 				size_t jmax = bj[0] + bstep[0] > size ? size : bj[0] + bstep[0];
 				size_t kmax = bk[0] + bstep[0] > size ? size : bk[0] + bstep[0];
-
 				for (size_t j = bj[0]; j < jmax; ++j) {
 					for (size_t i = bi[0]; i < imax; ++i) {
 						for (size_t k = bk[0]; k < kmax; ++k) {
@@ -124,21 +98,20 @@ void t_matrix_mult(size_t size){
 	asm("END Tiled0");
 	printf("Tiled0: %f sec\n", timer.tick(&cl));
 	if(PRINT_MATRIX) { printm(X); cout << endl; }
-	/**/
 	
 	
 	set(X,0);
 	timer.tick();
 	asm("Tiled1");
-	block(i2,0,size, j2,0,size, k2,0,size, bstep[1]){
-		size_t i1max = i2 + bstep[1] > size ? size : i2 + bstep[1];
-		size_t j1max = j2 + bstep[1] > size ? size : j2 + bstep[1];
-		size_t k1max = k2 + bstep[1] > size ? size : k2 + bstep[1];
-		block(i1,i2,i1max, j1,j2,j1max, k1,k2,k1max, bstep[0]){
-			size_t imax = i1 + bstep[0] > size ? size : i1 + bstep[0];
-			size_t jmax = j1 + bstep[0] > size ? size : j1 + bstep[0];
-			size_t kmax = k1 + bstep[0] > size ? size : k1 + bstep[0];
-			block(i,i1,imax, j,j1,jmax, k,k1,kmax, 1){
+	block(bi[1],0,size, bj[1],0,size, bk[1],0,size, bstep[1]){
+		size_t i1max = bi[1] + bstep[1] > size ? size : bi[1] + bstep[1];
+		size_t j1max = bj[1] + bstep[1] > size ? size : bj[1] + bstep[1];
+		size_t k1max = bk[1] + bstep[1] > size ? size : bk[1] + bstep[1];
+		block(bi[0],bi[1],i1max, bj[0],bj[1],j1max, bk[0],bk[1],k1max, bstep[0]){
+			size_t imax = bi[0] + bstep[0] > size ? size : bi[0] + bstep[0];
+			size_t jmax = bj[0] + bstep[0] > size ? size : bj[0] + bstep[0];
+			size_t kmax = bk[0] + bstep[0] > size ? size : bk[0] + bstep[0];
+			block(i,bi[0],imax, j,bj[0],jmax, k,bk[0],kmax, 1){
 				X.at(i, j) = X.at(i, j) + LU.at(i, k) * B.at(k, j);
 			}
 		}
@@ -151,19 +124,19 @@ void t_matrix_mult(size_t size){
 	set(X,0);
 	timer.tick();
 	asm("Tiled2");
-	block(i3,0,size, j3,0,size, k3,0,size, bstep[2]){
-		size_t i2max = i3 + bstep[2] > size ? size : i3 + bstep[2];
-		size_t j2max = j3 + bstep[2] > size ? size : j3 + bstep[2];
-		size_t k2max = k3 + bstep[2] > size ? size : k3 + bstep[2];
-		block(i2,i3,i2max, j2,j3,j2max, k2,k3,k2max, bstep[1]){
-			size_t i1max = i2 + bstep[1] > size ? size : i2 + bstep[1];
-			size_t j1max = j2 + bstep[1] > size ? size : j2 + bstep[1];
-			size_t k1max = k2 + bstep[1] > size ? size : k2 + bstep[1];
-			block(i1,i2,i1max, j1,j2,j1max, k1,k2,k1max, bstep[0]){
-				size_t imax = i1 + bstep[0] > size ? size : i1 + bstep[0];
-				size_t jmax = j1 + bstep[0] > size ? size : j1 + bstep[0];
-				size_t kmax = k1 + bstep[0] > size ? size : k1 + bstep[0];
-				block(i,i1,imax, j,j1,jmax, k,k1,kmax, 1){
+	block(bi[2],0,size, bj[2],0,size, bk[2],0,size, bstep[2]){
+		size_t i2max = bi[2] + bstep[2] > size ? size : bi[2] + bstep[2];
+		size_t j2max = bj[2] + bstep[2] > size ? size : bj[2] + bstep[2];
+		size_t k2max = bk[2] + bstep[2] > size ? size : bk[2] + bstep[2];
+		block(bi[1],bi[2],i2max, bj[1],bj[2],j2max, bk[1],bk[2],k2max, bstep[1]){
+			size_t i1max = bi[1] + bstep[1] > size ? size : bi[1] + bstep[1];
+			size_t j1max = bj[1] + bstep[1] > size ? size : bj[1] + bstep[1];
+			size_t k1max = bk[1] + bstep[1] > size ? size : bk[1] + bstep[1];
+			block(bi[0],bi[1],i1max, bj[0],bj[1],j1max, bk[0],bk[1],k1max, bstep[0]){
+				size_t imax = bi[0] + bstep[0] > size ? size : bi[0] + bstep[0];
+				size_t jmax = bj[0] + bstep[0] > size ? size : bj[0] + bstep[0];
+				size_t kmax = bk[0] + bstep[0] > size ? size : bk[0] + bstep[0];
+				block(i,bi[0],imax, j,bj[0],jmax, k,bk[0],kmax, 1){
 					X.at(i, j) = X.at(i, j) + LU.at(i, k) * B.at(k, j);
 				}
 			}
@@ -178,23 +151,23 @@ void t_matrix_mult(size_t size){
 	set(X,0);
 	timer.tick();
 	asm("Tiled3");
-	block(i4,0,size, j4,0,size, k4,0,size, bstep[3]){
-		size_t i3max = i4 + bstep[3] > size ? size : i4 + bstep[3];
-		size_t j3max = j4 + bstep[3] > size ? size : j4 + bstep[3];
-		size_t k3max = k4 + bstep[3] > size ? size : k4 + bstep[3];
-		block(i3,i4,i3max, j3,j4,j3max, k3,k4,k3max, bstep[2]){
-			size_t i2max = i3 + bstep[2] > size ? size : i3 + bstep[2];
-			size_t j2max = j3 + bstep[2] > size ? size : j3 + bstep[2];
-			size_t k2max = k3 + bstep[2] > size ? size : k3 + bstep[2];
-			block(i2,i3,i2max, j2,j3,j2max, k2,k3,k2max, bstep[1]){
-				size_t i1max = i2 + bstep[1] > size ? size : i2 + bstep[1];
-				size_t j1max = j2 + bstep[1] > size ? size : j2 + bstep[1];
-				size_t k1max = k2 + bstep[1] > size ? size : k2 + bstep[1];
-				block(i1,i2,i1max, j1,j2,j1max, k1,k2,k1max, bstep[0]){
-					size_t imax = i1 + bstep[0] > size ? size : i1 + bstep[0];
-					size_t jmax = j1 + bstep[0] > size ? size : j1 + bstep[0];
-					size_t kmax = k1 + bstep[0] > size ? size : k1 + bstep[0];
-					block(i,i1,imax, j,j1,jmax, k,k1,kmax, 1){
+	block(bi[3],0,size, bj[3],0,size, bk[3],0,size, bstep[3]){
+		size_t i3max = bi[3] + bstep[3] > size ? size : bi[3] + bstep[3];
+		size_t j3max = bj[3] + bstep[3] > size ? size : bj[3] + bstep[3];
+		size_t k3max = bk[3] + bstep[3] > size ? size : bk[3] + bstep[3];
+		block(bi[2],bi[3],i3max, bj[2],bj[3],j3max, bk[2],bk[3],k3max, bstep[2]){
+			size_t i2max = bi[2] + bstep[2] > size ? size : bi[2] + bstep[2];
+			size_t j2max = bj[2] + bstep[2] > size ? size : bj[2] + bstep[2];
+			size_t k2max = bk[2] + bstep[2] > size ? size : bk[2] + bstep[2];
+			block(bi[1],bi[2],i2max, bj[1],bj[2],j2max, bk[1],bk[2],k2max, bstep[1]){
+				size_t i1max = bi[1] + bstep[1] > size ? size : bi[1] + bstep[1];
+				size_t j1max = bj[1] + bstep[1] > size ? size : bj[1] + bstep[1];
+				size_t k1max = bk[1] + bstep[1] > size ? size : bk[1] + bstep[1];
+				block(bi[0],bi[1],i1max, bj[0],bj[1],j1max, bk[0],bk[1],k1max, bstep[0]){
+					size_t imax = bi[0] + bstep[0] > size ? size : bi[0] + bstep[0];
+					size_t jmax = bj[0] + bstep[0] > size ? size : bj[0] + bstep[0];
+					size_t kmax = bk[0] + bstep[0] > size ? size : bk[0] + bstep[0];
+					block(i,bi[0],imax, j,bj[0],jmax, k,bk[0],kmax, 1){
 						X.at(i, j) = X.at(i, j) + LU.at(i, k) * B.at(k, j);
 					}
 				}
@@ -205,32 +178,77 @@ void t_matrix_mult(size_t size){
 	printf("Tiled3: %f sec\n", timer.tick(&cl));
 	if(PRINT_MATRIX) { printm(X); cout << endl; }
 	
-	
-	size_t bkmax[5];
-	set(X,0);
+	////
+	// LU solving
+	////
+	set(X,B);
 	timer.tick();
 	asm("Tiled0");
-	for (size_t bi[0] = 0; bi[0] < size; bi[0] += bstep[0]) {
-		for (size_t bj[0] = 0; bj[0] < size; bj[0] += bstep[0]) {
-			bkmax[0] = bi[0];
-			bkmax[0] = bi[0] + bstep[0];
-			for (size_t bk[0] = 0; bk[0] < kmax[0]; bk[0] += bstep[0]) {
-				size_t imax = bi[0] + bstep[0] > size ? size : bi[0] + bstep[0];
-				size_t jmax = bj[0] + bstep[0] > size ? size : bj[0] + bstep[0];
-				size_t kmax = bk[0] + bstep[0] > size ? size : bk[0] + bstep[0];
-
-				for (size_t j = bj[0]; j < jmax; ++j) {
-					for (size_t i = bi[0]; i < imax; ++i) {
-						for (size_t k = bk[0]; k < kmax; ++k) {
-							X.at(i, j) = X.at(i, j) + LU.at(i, k) * B.at(k, j);
-						}
-					}
+	for (bi[0] = 0; bi[0] < size; bi[0] += bstep[0])
+	for (bj[0] = 0; bj[0] < size; bj[0] += bstep[0]) {
+		bkmax[0] = bi[0] + bstep[0];
+		for (bk[0] = 0; bk[0] < (bi[0]); bk[0] += bstep[0]) {
+			size_t imax = bi[0] + bstep[0] > size ? size : bi[0] + bstep[0];
+			size_t jmax = bj[0] + bstep[0] > size ? size : bj[0] + bstep[0];
+			size_t kmax = bk[0] + bstep[0];
+			block(i,bi[0],imax, j,bj[0],jmax, k,bk[0],kmax, 1){
+				X.at(i, j) = X.at(i, j) - LU.at(i, k) * B.at(k, j);
+			}
+		}
+		// Last block in K, diagonal
+		for (bk[0] = (bi[0]); bk[0] < (bi[0] + bstep[0]); bk[0] += bstep[0]) {
+			size_t imax = bi[0] + bstep[0] > size ? size : bi[0] + bstep[0];
+			size_t jmax = bj[0] + bstep[0] > size ? size : bj[0] + bstep[0];
+			for (size_t j = bj[0]; j < jmax; ++j)
+			for (size_t i = bi[0]; i < imax; ++i) {
+				for (size_t k = bk[0]; k < i; ++k) {
+					X.at(i, j) = X.at(i, j) - LU.at(i, k) * B.at(k, j);
 				}
+				X.at(i, j) /= LU.at(i, i);
 			}
 		}
 	}
 	asm("END Tiled0");
 	printf("Tiled0: %f sec\n", timer.tick(&cl));
+	if(PRINT_MATRIX) { printm(X); cout << endl; }
+	
+	
+	set(X,0);
+	timer.tick();
+	asm("Tiled1");
+	block(bi[1],0,size, bj[1],0,size, bk[1],0,(bi[1]+bstep[1]), bstep[1]){
+		bimax[0] = bi[1] + bstep[1] > size ? size : bi[1] + bstep[1];
+		bjmax[0] = bj[1] + bstep[1] > size ? size : bj[1] + bstep[1];
+		for (bi[0] = 0; bi[0] < size; bi[0] += bstep[0])
+		for (bj[0] = 0; bj[0] < size; bj[0] += bstep[0]) {
+			bkmax[0] = bi[0] + bstep[0];
+			for (bk[0] = 0; bk[0] < (bkmax[0] - bstep[0]); bk[0] += bstep[0]) {
+				size_t imax = bi[0] + bstep[0] > size ? size : bi[0] + bstep[0];
+				size_t jmax = bj[0] + bstep[0] > size ? size : bj[0] + bstep[0];
+				size_t kmax = bk[0] + bstep[0];
+				block(i,bi[0],imax, j,bj[0],jmax, k,bk[0],kmax, 1){
+					X.at(i, j) = X.at(i, j) - LU.at(i, k) * B.at(k, j);
+				}
+			}
+			// Last block in K, diagonal
+			for (bk[0] = (bi[0]); bk[0] < (bi[0] + bstep[0]); bk[0] += bstep[0]) {
+				size_t imax = bi[0] + bstep[0] > size ? size : bi[0] + bstep[0];
+				size_t jmax = bj[0] + bstep[0] > size ? size : bj[0] + bstep[0];
+				block(i,bi[0],imax, j,bj[0],jmax, k,bk[0],i, 1){
+					X.at(i, j) = X.at(i, j) - LU.at(i, k) * B.at(k, j);
+				}
+				for (size_t j = bj[0]; j < jmax; ++j)
+				for (size_t i = bi[0]; i < imax; ++i) {
+					for (size_t k = bk[0]; k < i; ++k) {
+						X.at(i, j) = X.at(i, j) - LU.at(i, k) * B.at(k, j);
+					}
+					X.at(i, j) /= LU.at(i, i);
+				}
+			}
+		}
+	}
+	asm("END Tiled1");
+	printf("Tiled1: %f sec\n", timer.tick(&cl));
 	if(PRINT_MATRIX) { printm(X); cout << endl; }
 	
 	

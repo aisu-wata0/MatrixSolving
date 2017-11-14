7f5da58412b4554942497e060b64d8102a4fb49e
lgjr15 <kartkork@gmail.com>
2017-11-14 17:21:44 -0200

    new script for tests

--- batch.sh
+++ batch.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-
 #32 33 64 65 128 129 256 257 512 1000 2000
 er() { echo "\$ $@" ; "$@" ; }
+sizes="32 33 64 65 128 129 256 257 512 1000 2000"
 
-for size in 512; do
+for size in $sizes; do
 	for version in "master" "optm" "tri" "loop_block" "mult_block"; do
-		er git checkout $version
-		er make rebuild
+		echo er git checkout $version
+		echo er make rebuild
 		er mv invmat invmat_${version}
 		for LIK_FLAG in "CACHE" "FLOPS_DP" "MEM"; do
 			for ((i=0; i<2; i++)); do
 				args="-r$size -i3 -oout_${i}_${version}.txt"
-				er ./lik.sh "${i}_${version}${LIK_FLAG}.txt" ${LIK_FLAG} "./MatrixSolving_${version}.exe $args"
-				# er ./invmat_${version} $args
+				echo er ./lik.sh "loglik${LIK_FLAG}_${version}_${i}.txt" ${LIK_FLAG} "./invmat $args"
 			done
 		done
 	done
 done
+
+#    Banda de Memória: utilizar o grupo MEM do LIKWID, e apresentar o resultado de "Memory bandwidth [MBytes/s]"; Caso não tenha o grupo MEM, utilize o grupo L3.
+#    Cache miss L1: utilizar o grupo CACHE do LIKWID, e apresentar o resultado de "data cache miss ratio". Caso não tenha o cache miss da L1, utilize o cache miss da L2.
+#    Operações aritméticas: utilizar o grupo FLOPS_DP ou FLOPS_AVX do LIKWID, e apresentar o resultado de "MFLOP/s"

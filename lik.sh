772e53c8a3fb9f280f13a2266f9088e06cf243c6
lgjr15 <kartkork@gmail.com>
2017-11-16 11:11:51 -0200

    Fix script

--- lik.sh
+++ lik.sh
@@ -1,2 +1,2 @@
-# ./lik.sh liklog.txt CACHE ./invmat -r512 -i4
-likwid-perfctr -C 1  -f  -o $1 -O -g $2 -m $3
+# ./lik.sh "liklog.txt" CACHE "./invmat -r512 -i4"
+likwid-perfctr -C 1  -f  -o "$1" -O -g $2 -m "$3"

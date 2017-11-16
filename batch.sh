276fbf34a51778f527e5df372fc9d6b45e5aca69
lgjr15 <kartkork@gmail.com>
2017-11-16 16:50:30 -0200

    pt1

--- batch.sh
+++ batch.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-#32 33 64 65 128 129 256 257 512 1000 2000
-er() { echo "\$ $@" ; "$@" ; }
-sizes="32 33 64 65 128 129 256 257 512 1000 2000"
+# ./batch.sh tests/
+# For each size, each version, each flag, in outDir create a directory for each flag, then create logs for each run inside the directory
 
-for size in $sizes; do
-	for version in "naive_lik" "mult_block_likwid"; do
-		er git checkout $version
-		er make rebuild
-		er mv invmat invmat_${version}
-		for LIK_FLAG in "L2CACHE" "FLOPS_DP" "MEM"; do
+er() { echo "\$ $@" ; "$@" ; }
+
+outDir=$1
+#"32 33 64 65 128 129 256 257 512 1000 2000"
+sizes="32 33 64 65 128 129 256 257 512 1000 2000"
+#"naive_lik" "mult_block_likwid"
+versions="naive_lik" "mult_block_likwid"
+
+for version in $versions; do
+	er git checkout $version
+	er make rebuild
+	er mv invmat invmat_${version}
+	
+	for size in $sizes; do
+		for LIK_FLAG in "L2CACHE" "FLOPS_DP" "L3"; do
 			args="-r$size -i10 -olog-time-${version}-${size}.txt"
-			er ./lik.sh "loglik-${LIK_FLAG}-${version}-${size}.txt" ${LIK_FLAG} "./invmat $args"
+			mkdir -p $outDir/$LIK_FLAG
+			er ./lik.sh "$outDir/${LIK_FLAG}/liklog-${LIK_FLAG}-${version}-${size}.txt" ${LIK_FLAG} "./invmat_${version} $args"
 		done
 	done
 done
